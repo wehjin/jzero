a8aed8afa18fc4467cc6f95e7ec197c715852c51
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2018-04-07 19:23:58 -0700

    Open patchgl window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+extern crate patchgl;
+
 fn main() {
-    println!("Hello, world!");
+    window::start(640, 400, |window| {
+        use patchgl::app::App;
+        use self::jzero::*;
+
+        App::new(update, draw)
+            .run("Jzero", Mdl::default(), window);
+    });
+    use patchgl::window;
+}
+
+mod jzero {
+    pub fn update(mdl: &mut Mdl, msg: Msg) {}
+
+    #[derive(Clone, PartialEq, Debug)]
+    pub struct Mdl {}
+
+    impl Default for Mdl {
+        fn default() -> Self {
+            Mdl {}
+        }
+    }
+
+    #[derive(Copy, Clone, Eq, PartialEq, Debug)]
+    pub enum Msg {}
+
+    pub fn draw(model: &Mdl) -> Flood<Msg> {
+        let palette = Palette::default();
+        use patchgl::material::Palette;
+        Flood::Color(palette.light_background)
+    }
+
+    use patchgl::flood::*;
 }
