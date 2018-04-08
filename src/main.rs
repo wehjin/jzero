4d1da221dca77c11390009d61d955e51b978eb92
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2018-04-07 19:42:32 -0700

    Add draw_performance

--- src/main.rs
+++ src/main.rs
@@ -12,25 +12,56 @@ fn main() {
 }
 
 mod jzero {
-    pub fn update(mdl: &mut Mdl, msg: Msg) {}
+    pub fn update(_mdl: &mut Mdl, _msg: Msg) {}
 
     #[derive(Clone, PartialEq, Debug)]
-    pub struct Mdl {}
+    pub struct Mdl {
+        card: RecallCard,
+        view_state: ViewState,
+    }
+
+    #[derive(Clone, PartialEq, Debug)]
+    pub struct RecallCard {
+        pub english: String,
+        pub progressive: String,
+        pub kana: String,
+        pub kanji: Option<String>,
+    }
+
+    #[derive(Clone, PartialEq, Debug)]
+    pub enum ViewState {
+        Performance,
+    }
 
     impl Default for Mdl {
         fn default() -> Self {
-            Mdl {}
+            let card = RecallCard {
+                english: "mouth".into(),
+                progressive: "kuchi".into(),
+                kana: "くち".into(),
+                kanji: Some("口".into()),
+            };
+            let view_state = ViewState::Performance;
+            Mdl { card, view_state }
         }
     }
 
     #[derive(Copy, Clone, Eq, PartialEq, Debug)]
     pub enum Msg {}
 
-    pub fn draw(model: &Mdl) -> Flood<Msg> {
+    pub fn draw(mdl: &Mdl) -> Flood<Msg> {
         let palette = Palette::default();
-        use patchgl::material::Palette;
-        Flood::Color(palette.light_background)
+        let card = match mdl.view_state {
+            ViewState::Performance => draw_performance(&palette, &mdl.card)
+        };
+        card + Flood::Color(palette.light_background)
     }
 
+    use patchgl::material::Palette;
     use patchgl::flood::*;
+
+    fn draw_performance(palette: &Palette, card: &RecallCard) -> Flood<Msg> {
+        Flood::Text(card.english.to_owned(), palette.light_background_text_primary, Placement::Start)
+            + Padding::Uniform(Length::Cross * 0.4)
+    }
 }
