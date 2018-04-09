1074f169171339962d5bee30ecd9ba8ad26a5358
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2018-04-09 10:53:07 -0700

    Added remainder of vocabulary group a.

--- src/jzero/mod.rs
+++ src/jzero/mod.rs
@@ -58,7 +58,62 @@ impl Default for JzeroMdl {
                     progressive: "kuchi".into(),
                     kana: "くち".into(),
                     kanji: Some("口".into()),
-                }],
+                },
+                Question::Recall {
+                    english: "eye".into(),
+                    progressive: "me".into(),
+                    kana: "め".into(),
+                    kanji: Some("目".into()),
+                },
+                Question::Recall {
+                    english: "ear".into(),
+                    progressive: "mimi".into(),
+                    kana: "みみ".into(),
+                    kanji: Some("耳".into()),
+                },
+                Question::Recall {
+                    english: "nose".into(),
+                    progressive: "hana".into(),
+                    kana: "はな".into(),
+                    kanji: Some("鼻".into()),
+                },
+                Question::Recall {
+                    english: "face".into(),
+                    progressive: "kao".into(),
+                    kana: "かお".into(),
+                    kanji: Some("顔".into()),
+                },
+                Question::Recall {
+                    english: "hand".into(),
+                    progressive: "te".into(),
+                    kana: "て".into(),
+                    kanji: Some("手".into()),
+                },
+                Question::Recall {
+                    english: "foot, leg".into(),
+                    progressive: "ashi".into(),
+                    kana: "あし".into(),
+                    kanji: Some("足".into()),
+                },
+                Question::Recall {
+                    english: "finger".into(),
+                    progressive: "yubi".into(),
+                    kana: "ゆび".into(),
+                    kanji: Some("指".into()),
+                },
+                Question::Recall {
+                    english: "head".into(),
+                    progressive: "atama".into(),
+                    kana: "あたま".into(),
+                    kanji: Some("頭".into()),
+                },
+                Question::Recall {
+                    english: "tooth, teeth".into(),
+                    progressive: "ha".into(),
+                    kana: "は".into(),
+                    kanji: Some("歯".into()),
+                },
+            ],
             active_lesson: Lesson {
                 question: Question::Recall {
                     english: "mouth".into(),
