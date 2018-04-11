cb3798602a9146e88dfd93a38cce23931f11965a
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2018-04-10 21:57:41 -0700

    Move section module into ui module

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,11 @@ extern crate time;
 use patchgl::app::App;
 use patchgl::traits::*;
 use patchgl::window;
-use session::SessionMdl;
+use ui::section::SessionMdl;
 
-mod session;
 mod storage;
 mod domain;
+mod ui;
 
 fn main() {
     window::start(768, 768, |window| {
